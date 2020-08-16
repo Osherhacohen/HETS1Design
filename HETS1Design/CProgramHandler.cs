55f6882f46e1f0566db398b714767542d90ffe22
Osher <osherhacohen@gmail.com>
2020-08-16 18:58:05 +0300

    Added some classes.

---
+++ HETS1Design/CProgramHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HETS1Design
+{
+    static class CProgramHandler
+    {
+        public static void CompileCode(string codeFilePath)
+            {}
+        public static void RunEXE(string codeFilePath, string inFilePath)
+            {}
+    }
+}
---
+++ HETS1Design/Submission.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HETS1Design
+{
+    class Submission
+    {
+        string submissionPath;
+        string submitID;
+        string codePath; //It's either both .c/exe or 
+        string exePath; //just one of them.
+        string[] output; //Output per test case.
+
+        double grade; //?? maybe, may do per correct output line.
+    }
+}
