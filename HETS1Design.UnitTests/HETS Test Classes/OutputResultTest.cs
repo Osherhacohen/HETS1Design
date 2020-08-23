aa045b2fb8b530dfe7eeb5660c22734a1d5d3fe6
zemach jacob <45815373+CHAOS-en-Krojerk@users.noreply.github.com>
2020-08-23 18:35:08 +0300

    OutputResults test units

--- HETS1Design.UnitTests/HETS Test Classes/OutputResultTest.cs
+++ HETS1Design.UnitTests/HETS Test Classes/OutputResultTest.cs
@@ -6,9 +6,27 @@ namespace HETS1Design
     [TestClass]
     public class OutputResultTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        OutputResult o1;
+
+        [TestInitialize]
+        public void Initialize()
         {
+            this.o1 = new OutputResult("something");
         }
+
+        [TestMethod]
+        public void Match_True()
+        {
+            o1.Match();
+            Assert.IsTrue(o1.DidItMatch);
+        }
+
+        [TestMethod]
+        public void Mismatch_NotTrue()
+        {
+            o1.Mismatch();
+            Assert.IsFalse(o1.DidItMatch);
+        }
+
     }
 }
