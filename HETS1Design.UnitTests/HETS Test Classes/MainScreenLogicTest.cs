e6f015aebd7c3f043c0eb6c224dc1ea0942a39dd
Osher <osherhacohen@gmail.com>
2020-08-24 15:15:16 +0300

    a

---
+++ HETS1Design.UnitTests/HETS Test Classes/MainScreenLogicTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HETS1Design.UnitTests
+{
+    [TestClass]
+    public class MainScreenLogicTest
+    {
+        [TestMethod]
+        public void TestFormValidate()
+        {
+        }
+    }
+}
