db8c5e0dbe43e8db45c086ff05e433d49cf03374
zemach jacob <45815373+CHAOS-en-Krojerk@users.noreply.github.com>
2020-08-26 15:11:57 +0300

    Added tests for ZipHandler

--- HETS1Design.UnitTests/HETS Test Classes/ZipArchiveHandlerTest.cs
+++ HETS1Design.UnitTests/HETS Test Classes/ZipArchiveHandlerTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HETS1Design
@@ -7,8 +8,15 @@ namespace HETS1Design
     public class ZipArchiveHandlerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ZipArchiveHandler_Success()
         {
+            string filePath = @"..\..\..\Assets\Test Required FIles\ZipArchiveHandlerTest\ZipForTest.zip";
+            ZipArchiveHandler.GetSubmissionData(filePath, true);
+            Assert.IsTrue(File.Exists(@"..\..\..\Assets\Test Required FIles\ZipArchiveHandlerTest\Codes To Check\איימן שפסו_7085_assignsubmission_file_\Q3 .c"));
+            Assert.IsTrue(File.Exists(@"..\..\..\Assets\Test Required FIles\ZipArchiveHandlerTest\Codes To Check\אחמד סכראן_7092_assignsubmission_file_\mtla3.c"));
+            Assert.IsTrue(File.Exists(@"..\..\..\Assets\Test Required FIles\ZipArchiveHandlerTest\Codes To Check\אחמד סכראן_7092_assignsubmission_file_\code files\erl.h"));
+            Assert.IsTrue(File.Exists(@"..\..\..\Assets\Test Required FIles\ZipArchiveHandlerTest\Codes To Check\אחמד סכראן_7092_assignsubmission_file_\code files\Targil5a.c"));
+            Assert.IsTrue(File.Exists(@"..\..\..\Assets\Test Required FIles\ZipArchiveHandlerTest\Codes To Check\אחמד סכראן_7092_assignsubmission_file_\Exe\Source.exe"));
         }
     }
 }
